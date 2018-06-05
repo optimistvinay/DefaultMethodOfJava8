ef676d27c7f6c900b009b4680273d526d3a364a9
MSDOMAIN1\VSawant <vinayak.sawant@morningstar.com>
2018-06-05 17:19:48 +0530

    test 5 check in

--- src/TestingPR/TestClass.java
+++ src/TestingPR/TestClass.java
@@ -5,6 +5,7 @@ import java.util.Calendar;
 public class TestClass {
 	public static void main(String[] args) {
 		int year = 1;
+
 		
 		int currentYear = Calendar.getInstance().get(Calendar.YEAR);
 		int currentYearDigit = (currentYear - currentYear / 100 * 100) / 10;
